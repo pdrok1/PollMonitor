981bb0e2b5b6b65dbf10e2c2c67cef4729ff895e
Pedro <pedroqueiroz75@hotmail.com>
2020-10-06 16:40:58 -0300

    Initial Commit

---
+++ Controllers/PollController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using PollMonitor.Repository;
+using System.Linq;
+
+namespace PollMonitor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PollController : ControllerBase
+    {
+        private readonly ApplicationDbContext _database;
+        public PollController(ApplicationDbContext database)
+        {
+            this._database = database;
+        }
+
+        [HttpGet]
+        public IActionResult getPolls() 
+        {
+            //
+            string r = "{ polls: [ " +  + " ] } ";
+            Response.ContentType = "application/json; charset=utf-8";
+            return new JsonResult(r);
+        }
+    }
+}
---
+++ Repository/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PollMonitor.Repository
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        //public DbSet<visitantes> Visitantes { get; set; }
+    }
+}
